856d5428e305709eb56e031432d9382b8c47f374
Kwon HK <klw95@naver.com>
2020-07-27 23:48:05 +0900

    remove profile page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,6 @@ import "assets/scss/material-kit-react.scss?v=1.9.0";
 
 // pages for this product
 import Components from "views/Components/Components.js";
-import Profile from "views/ProfilePage/ProfilePage.js";
 import SearchTeam from "views/SearchTeamPage/SearchTeamPage.js";
 
 var hist = createBrowserHistory();
@@ -16,7 +15,6 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/searchteam" component={SearchTeam} />
-      <Route path="/profile" component={Profile} />
       <Route path="/" component={Components} />
     </Switch>
   </Router>,
