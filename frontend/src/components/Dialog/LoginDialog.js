41e15226f7464311c7da32d2abf61017706a012a
김형준 <gudwns026@naver.com>
2020-07-29 16:05:17 +0900

    Modify login ui

--- frontend/src/components/Dialog/LoginDialog.js
+++ frontend/src/components/Dialog/LoginDialog.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-import { Dialog, DialogTitle, Divider, TextField } from "@material-ui/core";
+import { Dialog, DialogTitle } from "@material-ui/core";
 
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-
-import Button from "@material-ui/core/Button";
 
 import { GoogleLogin } from "react-google-login";
 import KakaoLogin from "react-kakao-login";
