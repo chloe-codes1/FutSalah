19d6013f7b430b6b14c19e8bc590f1713a73087e
Hyun <yodg3456@naver.com>
2020-08-11 01:50:25 +0900

    S03P13A112-42 add search matching list

---
+++ backend/src/main/java/ido/arduino/dto/MatchSimpleDto.java
@@ -0,0 +1,19 @@
+package ido.arduino.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MatchSimpleDto {
+
+	private Date date ;
+	private int locationID;
+	
+}
+
