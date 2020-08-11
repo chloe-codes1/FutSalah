95543812a604a7729b4de9ed72affdc01d63cc4b
김동휘 <wheeking@gmail.com>
2020-08-11 10:52:15 +0900

    S03P13A112-6 | Complete CourtLogin

---
+++ backend/src/main/java/ido/arduino/dto/CourtAdminDTO.java
@@ -0,0 +1,17 @@
+package ido.arduino.dto;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Alias("CourtAdminDTO")
+public class CourtAdminDTO {
+	private int adminID;
+	private String id;
+	private int stadiumID;
+}
