9acd6bf812b70a7deeece525cb3bc4762611853d
Juhyun Kim <kimjuhyun825@naver.com>
2020-08-11 14:56:15 +0900

    Add getLocationBySido API

--- backend/src/main/java/ido/arduino/controller/LocationController.java
+++ backend/src/main/java/ido/arduino/controller/LocationController.java
@@ -1,11 +1,14 @@
 package ido.arduino.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -28,4 +31,11 @@ public class LocationController {
 	public @ResponseBody List<LocationDto> getAllLocationInfo(){
 		return locationService.getAllLocationInfo();
 	}
+	
+	@PostMapping("/location")
+	public @ResponseBody List<LocationDto> getLocationBySido(@RequestBody Map<String, String> data){
+		String sido = data.get("sido");
+		return locationService.getLocationBySido(sido);
+	}
+	
 }
