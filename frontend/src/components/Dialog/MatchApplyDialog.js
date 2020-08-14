0d735c0abadc8095414d4604a016a0a7b26a4567
김형준 <gudwns026@naver.com>
2020-08-15 02:05:00 +0900

    S03P13A112-55 | Add matchapply UI

--- frontend/src/components/Dialog/MatchApplyDialog.js
+++ frontend/src/components/Dialog/MatchApplyDialog.js
@@ -1,14 +1,34 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import Button from "components/CustomButtons/Button.js";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
 
-function MatchApplyDialog() {
-  return <div>MatchApplyDialog</div>;
+function MatchApplyDialog({ open, onClose, info }) {
+  return (
+    <div>
+      <Dialog open={open} onClose={onClose}>
+        <DialogTitle>매치 상세정보</DialogTitle>
+        <DialogContent>
+          <DialogContentText>경기방식 : {info.formCode}인 팀 매치</DialogContentText>
+          <DialogContentText>경기일시 : {info.date}</DialogContentText>
+          <DialogContentText>지역 : {info.gu}</DialogContentText>
+          {info.isBooked === 1 && <DialogContentText>경기장 : {info.name}</DialogContentText>}
+          {info.isBooked === 0 && <DialogContentText>경기장 : 미정</DialogContentText>}
+          <Grid container justify="center">
+            <Grid item>
+              <Typography></Typography>
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
 
 export default MatchApplyDialog;
