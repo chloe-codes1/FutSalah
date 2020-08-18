25a87802d849f53337fa1401ca8236c6d8b354fc
Donghwi Kim <wheeking@gmail.com>
2020-08-18 23:01:47 +0900

    S03P13A112-60 | Create gameStart API

--- backend/src/main/java/ido/arduino/controller/CourtController.java
+++ backend/src/main/java/ido/arduino/controller/CourtController.java
@@ -10,7 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
 import ido.arduino.dto.CourtAdminDTO;
+import ido.arduino.dto.QRInfoDTO;
 import ido.arduino.service.CourtService;
 import io.swagger.annotations.ApiOperation;
 
@@ -32,6 +39,65 @@ public class CourtController {
 			return courtService.login(id);
 		} else {
 			return null;
+		}
+	}
+	
+	@ApiOperation(value="게임 시작", response = String.class)
+	@PostMapping("/gameStart")
+	public void gameStart(@RequestBody QRInfoDTO data) throws JSchException {
+		// QR 지각 관련
+		
+		/////
+		
+		
+		String homeTeam = "team1";
+		String awayTeam = "team2";
+		
+		String name = "pi";
+		String host = "192.168.0.9";
+		int port = 22;
+		String password = "root";
+
+		System.out.println("==> Connecting to " + host);
+		Session session = null;
+		Channel channel = null;
+
+		try {
+			// JSch 객체 생성
+			JSch jsch = new JSch();
+			session = jsch.getSession(name, host, port);
+
+			// 패스워드 설정
+			session.setPassword(password);
+
+			// 세션 관련 정보 설정
+			java.util.Properties config = new java.util.Properties();
+			// 호스트 정보 검사 x
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+
+			// 접속
+			session.connect();
+
+			// sftp 채널 오픈
+			channel = session.openChannel("exec");
+
+			// SSH용 채널 객체로 캐스팅
+			ChannelExec channelExec = (ChannelExec) channel;
+			System.out.println("==> Connected to " + host);
+
+			channelExec.setCommand("python3 /home/pi/Desktop/goal.py " + homeTeam + " " + awayTeam);
+
+			channelExec.connect();
+			System.out.println("==> Connected to " + host);
+
+		} finally {
+			if (channel != null) {
+				channel.disconnect();
+			}
+			if (session != null) {
+				session.disconnect();
+			}
 		}
 	}
 }
