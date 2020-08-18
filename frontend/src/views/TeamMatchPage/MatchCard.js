e30b9a18bd279a03f161cfee725740acad9fc57b
김형준 <gudwns026@naver.com>
2020-08-18 19:47:08 +0900

    S03P13A112-55 | Modify matchcard ui

--- frontend/src/views/TeamMatchPage/MatchCard.js
+++ frontend/src/views/TeamMatchPage/MatchCard.js
@@ -89,14 +89,10 @@ export default function MatchCard({ match, myteam, setMyteam, userinfo }) {
     profileURL = process.env.REACT_APP_S3_BASE_URL + "/" + profileURL;
   } else {
     profileURL =
-<<<<<<< HEAD
       process.env.REACT_APP_S3_BASE_URL +
       "/team-default-" +
-      Math.ceil(Math.random(1, 8)) +
+      Math.ceil(Math.random() * 8) +
       ".png";
-=======
-      process.env.REACT_APP_S3_BASE_URL + "/team-default-" + Math.ceil(Math.random()*8) + ".png";
->>>>>>> 6ffb568f663a6e64d3ec459c2a59121c2d8e6cc0
   }
   console.log(profileURL);
   return (
